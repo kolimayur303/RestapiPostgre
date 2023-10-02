a767149a8aeb801aa30ced6bc51bce9d0fac12ae
Mayur Koli <mkoli@delaplex.com>
2023-10-02 14:24:11 +0530

    third commit postgre api

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const studentRoutes = require('./src/student/routes');
 const port = 3000;
 
 app.use(express.json());
-
-app.get("/", (req,res)=>{
-    res.send("hello world");
-});
+app.use(express.urlencoded( {extended:false}));
 
 app.use('/api/student', studentRoutes);
+
 app.listen(port, ()=> console.log(`app listening on port ${port}`));
